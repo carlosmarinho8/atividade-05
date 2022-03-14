f28a5bf0baa72180351feb8f9ebb863e8514ec00
devmarinhocarlos <devcarlosmariho@outlook.com>
2022-03-14 11:43:15 -0300

    app js done

--- react-redux-crud/src/App.js
+++ react-redux-crud/src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import AddClient from "./components/add-client.component";
+import Client from "./components/client.component";
+import ClientsList from "./components/clients-list.component";
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <nav className="navbar navbar-expand navbar-dark bg-dark">
+          <Link to={"/clients"} className="navbar-brand">
+            Agency Without Borders
+          </Link>
+          <div className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <Link to={"/clients"} className="nav-link">
+                Clients
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to={"/add"} className="nav-link">
+                Add
+              </Link>
+            </li>
+          </div>
+        </nav>
+        <div className="container mt-3">
+          <Switch>
+            <Route exact path={["/", "/clients"]} component={ClientsList} />
+            <Route exact path="/add" component={AddClient} />
+            <Route path="/clients/:id" component={Client} />
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
 }
-
 export default App;
